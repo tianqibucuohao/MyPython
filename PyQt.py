0f0424d156712c0f7e1e3ceb9357037dba64821e
lich <lichzhenglz@gmail.com>
2018-12-04 14:54:02 +0800

    rename

---
+++ PyQt.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 30 16:45:27 2018
+
+@author: Administrator
+"""
+
+import PyQt5
+
+print("11")
---
+++ listfile.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  3 11:35:52 2018
+
+@author: Administrator
+"""
+
+import os.path
+
+def main():
+    print(os.getcwd())
+    
+
+if (__name__ == "__main__"):
+    main()
