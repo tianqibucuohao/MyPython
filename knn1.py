eb40baf4254af8e5f708f26f380211d4e0501092
lich <lichzhenglz@gmail.com>
2018-12-06 17:47:03 +0800

    Update knn1.py

--- knn1.py
+++ knn1.py
@@ -22,7 +22,7 @@ plt.scatter(red[:,0],red[:,1],80,'r','^')
 
 blut = traindata[responses.ravel() ==1]
 print(blut)
-plt.scatter(blut[:,0],blut[:,1],80,'b','s')
+plt.scatter(blut[:,0],blut[:,1],90,'b','s')
 
 nc = py.random.randint(0,100,(1,2)).astype(py.float32)
 plt.scatter(nc[:,0], nc[:,1], 80,'g','o')
