2359133360544469daf02ad62a104722439e2ce0
lich <lichzhenglz@gmail.com>
2018-12-11 17:47:23 +0800

    Update knn2.py

--- knn2.py
+++ knn2.py
@@ -14,8 +14,10 @@ def demo():
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     # Now we split the image to 5000 cells, each 20x20 size
     cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
+    print(len(cells))
     # Make it into a Numpy array. It size will be (50,100,20,20)
     x = np.array(cells)
+    print(len(x))
     # Now we prepare train_data and test_data.
     train = x[:,:50].reshape(-1,400).astype(np.float32) # Size = (2500,400)
     test = x[:,50:100].reshape(-1,400).astype(np.float32) # Size = (2500,400)
@@ -26,7 +28,7 @@ def demo():
     # Initiate kNN, train the data, then test it with test data for k=1
     knn = cv2.ml.KNearest_create()
     knn.train(train, cv2.ml.ROW_SAMPLE, train_labels)
-    ret,result,neighbours,dist = knn.findNearest(test,k=5)
+    ret,result,neighbours,dist = knn.findNearest(test,k=4)
     # Now we check the accuracy of classification
     # For that, compare the result with test_labels and check which are wrong
     matches = result==test_labels
