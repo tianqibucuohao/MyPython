6faba90a43db7584c5f98127b27c0b45cf7ba7bd
lich <lichzhenglz@gmail.com>
2018-12-17 17:21:36 +0800

    1

---
+++ algorithm/test1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""
+斐波那契数列（Fibonacci sequence）
+"""
+
+def fs(n):
+    if (n < 2):
+        return 1
+    else:
+        fsum = fs(n-1)+fs(n-2)
+        return fsum
+
+def fbGen(n):
+    """
+    有问题只打印下标
+    """
+    a, b = 0, 1
+    while n>0:
+        #yield a
+        a, b, n = b, a+b, n-1
+
+def main():
+    """
+    2种赋值写法
+    """
+    if (1):
+        ls = []
+        for i in range(6):
+            ls.append(fs(i))
+    else:
+        ls = [i for i in range(6)]
+    print(ls)
+    l=[i for i in fbGen(6)]
+    print(l)
+
+if (__name__ == "__main__"):
+    main()
--- oo.py
+++
@@ -1,4 +0,0 @@
-import cv2
-
-print("%s" % cv2.cv2.__version__)
-#dir(cv2)
