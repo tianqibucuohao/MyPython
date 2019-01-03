3c2d6af537dd63b75fe71944cd2707f43fcacde5
lich <lichzhenglz@gmail.com>
2019-01-03 16:35:57 +0800

    Update t7.py

--- mc/t7.py
+++ mc/t7.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
 
-import cv2
+import cv2 as cv
 # 加载模型
-net = cv2.dnn.readNetFromTorch('./models/instance_norm/the_scream.t7')
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV);
+net = cv.dnn.readNetFromTorch('./models/instance_norm/the_scream.t7')
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV);
+
 # 读取图片
-image = cv2.imread('bra.jpg')
-(h, w) = image.shape[:2]
-blob = cv2.dnn.blobFromImage(image, 1.0, (w, h), (103.939, 116.779, 123.680), swapRB=False, crop=False)
-# 进行计算
-net.setInput(blob)
+cv.namedWindow('Styled image', cv.WINDOW_NORMAL)
+frame = cv.imread('TIM.png')
+(inWidth,inHeight) = frame.shape[:2]
+inp = cv.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight),
+                          (103.939, 116.779, 123.68), swapRB=False, crop=False)
+
+net.setInput(inp)
 out = net.forward()
+
 out = out.reshape(3, out.shape[2], out.shape[3])
-out[0]  = 103.939
-out[1]  = 116.779
-out[2]  = 123.68
+out[0] += 103.939
+out[1] += 116.779
+out[2] += 123.68
 out /= 255
 out = out.transpose(1, 2, 0)
-# 输出图片
-cv2.imshow('Styled image', out)
-cv2.waitKey(0)
+
+t, _ = net.getPerfProfile()
+freq = cv.getTickFrequency() / 1000
+print(t / freq, 'ms')
+
+cv.imshow('Styled image', out)
