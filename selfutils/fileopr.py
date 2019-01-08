ac0353322368198b579564b3c7e3d1b921e22a9b
lich <lichzhenglz@gmail.com>
2019-01-08 17:41:13 +0800

    Update fileopr.py

--- selfutils/fileopr.py
+++ selfutils/fileopr.py
@@ -7,24 +7,31 @@ Created on Mon Jan  7 17:22:07 2019
 file operation
 
 """
+import codecs
 
 def openfile(file):
     bIsUtf8 = False
-    with open(file,"rb") as f:
-        f1 = f.read(2)
-        print(len(f1))
-        ff1 = f1[0]
-        ff2 = f1[1]
+    with codecs.open(file,'r','utf-8') as f:
+        fbom = f.read(2)
+        print(len(fbom))
+        ff1 = fbom[0]
+        ff2 = fbom[1]
         print(ff1,ff2)
         # 0xff 0xfe
-        if (ff1 == 0xff & ff2 == 0xfe):
+        if (ff1 == 255 and ff2 == 254):
             bIsUtf8 = True
+        s=repr(f.read(200))
+        print(type(s))
+        if (bIsUtf8):
+            print(s)
     print("is utf-8:", bIsUtf8)        
 
 def main():
-    file1='../pachong/jux962.torrent'
+    file1='../pachong/hjd2048.com-1109ipx229-h264.torrent'
     file2='../pachong/a2.html'
     openfile(file2)
 
 if (__name__ == "__main__"):
+    help(open)
     main()
+    #help(str)
