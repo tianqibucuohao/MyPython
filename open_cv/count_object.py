ed15d4c668bb3aefa678712eca19f95a32038b57
lich <lichzhenglz@gmail.com>
2019-01-17 22:43:45 +0800

    0

--- open_cv/count_object.py
+++ open_cv/count_object.py
@@ -9,10 +9,13 @@ Created on Thu Jan 17 16:29:43 2019
 import cv2 as cv
 
 def main():
-    img = cv.imread('blox.jpg')
+    img = cv.imread('bra.jpg')
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    edge = cv.Canny(gray,10,100)
-    cv.imshow('edge',edge)
+    edge = cv.Canny(gray,100,150)
+    mo='edge'
+    cv.namedWindow(mo, cv.WINDOW_NORMAL)
+    cv.resizeWindow(mo, 200,200)
+    cv.imshow(mo,edge)
     
 
 if (__name__ == "__main__"):
