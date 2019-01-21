977cee15c0d2e2522eb304f2b188d6b1261788fb
lich <lichzhenglz@gmail.com>
2019-01-21 17:43:00 +0800

    Update getInfoFrombs.py

--- getInfoFrombs.py
+++ getInfoFrombs.py
@@ -26,18 +26,20 @@ def main():
     port = 8080
     path = "/DrcomSrv/DrcomService?"
     dicts = dict()
-    dicts['code'] = 085
+    dicts['code'] = "085"
     dicts["account"] = username
     dicts["realtime_flag"] = 0
     #obj = {"code" : 085,"account" : repr(username),"realtime_flag" : 0}
-    #obj = {"code":"001","account":"test","password":"123456","package_group_id":"1","serial_number":"20161111123433"}
+    obj = {"code":"001","account":"test","password":"123456","package_group_id":"1","serial_number":"20161111123433"}
     js = json.dumps(obj,ensure_ascii=False)
     print(js)
     #eyJjb2RlIjoiMDAxIiwiYWNjb3VudCI6InRlc3QiLCJwYXNzd29yZCI6IjEyMzQ1NiIsInBhY2thZ2VfZ3JvdXBfaWQiOiIxIiwic2VyaWFsX251bWJlciI6IjIwMTYxMTExMTIzNDMzIn0=
     b64 = base64.standard_b64encode(bytes(js, encoding='utf-8'))
-    print(repr(b64))
-    md5 = hashlib.md5(b64).hexdigest()
-    print(md5)
+    b64 = repr(b64)
+    b64 += 'abcdefg123'
+    print(b64)
+    md5 = "123"#hashlib.md5(bytes(b64)).hexdigest()
+    print("md5 =",md5)
     param = "iusername={"+username+"}"+"&business="
     
     
