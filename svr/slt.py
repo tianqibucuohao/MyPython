384a9b60153ca0b9979a94e69afeba0ee2ebbc15
lich <lichzhenglz@gmail.com>
2019-03-04 15:01:06 +0800

    Update slt.py

--- svr/slt.py
+++ svr/slt.py
@@ -26,32 +26,43 @@ content  <class 'str'> ,len= 485 ,method= GET , path= /jwo/hoj/jojl/oooo?jowj=23
 httpheader:HTTP/1.1\r\nHost: 127.0.0.1:8088\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: zh-CN,zh;q=0.9\r\n\r\n
 """
 
-class Respone:
-    def __init__(self, r):
+class Response:
+    def __init__(self):
         #不带content-length
-        self.respone = "HTTP/1.0 200 OK\r\nSERVER: Dr.COM VER SERVER\r\nCONTENT-TYPE: %s\r\nCONECTION: CLOSE\r\n\r\n"
+        self.respone = "HTTP/1.0 200 OK\r\nSERVER: Dr.COM VER SERVER\r\nCONTENT-LENGTH: {}\r\nCONTENT-TYPE: {}\r\nCONECTION: CLOSE\r\n\r\n{}"
         #带content-length
         #self.respone = "HTTP/1.0 200 OK\r\nSERVER: Dr.COM VER SERVER\r\nCONTENT-TYPE: %s\r\nCONTENT-LENGTH: %d\r\nCONECTION: CLOSE\r\n\r\n"
-        self.contenttype='Content-Type: application/json; charset=utf-8'
+        self.contenttype='application/json; charset=utf-8'
+        obj = {"code":"001","account":"test","password":"123456","package_group_id":"1","serial_number":"20161111123433"}
+        self.data = json.dumps(obj,ensure_ascii=False)
+    def GetResponse(self):
+        res =self.respone.format(len(self.data),self.contenttype, self.data)
+        #{self.contenttype, self.data})
+        return res
+    
 class Request:
     def __init__(self, r):
         print('Request:',r)
-        #整个http内容
-        self.content = r
-        # GET /POST ..
-        self.method = r.split(' ')[0]
-        # /jojoj
-        self.path = r.split(' ')[1].split('?')[0]
-        # headers 
-        self.header = self.GetHeaders()
-        # ?ojo&wfe&wjo=wef&234
-        self.pathParam=r.split()[1].split('?')[1].split('&')
-        self.param = self._parse_parmeter(self.pathParam)
-        print('method=', self.method
-              ,', path=', self.path
-              ,', param=', self.param)
-        #print('headers', self.header)
-#        self.body = r.split('\r\n\r\n', 1)[1]
+        try:
+            #整个http内容
+            self.content = r
+            # GET /POST ..
+            self.method = r.split(' ')[0]
+            # /jojoj
+            self.path = r.split(' ')[1].split('?')[0]
+            # headers 
+            self.header = self.GetHeaders()
+            # ?ojo&wfe&wjo=wef&234
+            self.pathParam=r.split()[1].split('?')[1].split('&')
+            #print('path param:', self.pathParam)
+            #self.param = self._parse_parmeter(self.pathParam)
+            print('method=', self.method
+                  ,', path=', self.path
+                  ,', param=', self.pathParam)
+            #print('headers', self.header)
+    #        self.body = r.split('\r\n\r\n', 1)[1]
+        except IndexError:
+            print("index error")
 
     def GetHeaders(self):
         header_content = self.content.split('\r\n\r\n')[0].split('\r\n')[1:]
@@ -60,10 +71,10 @@ class Request:
         for line in header_content:
             k, v = line.split(': ')
             result[quote(k)] = quote(v)
-        print('result:',result)
+        #print('result:',result)
         return result
 
-    def _parse_parameter(parameters):
+    def _parse_parmeter(parameters):
         args = parameters.split('&')
         query = {}
         for arg in args:
@@ -86,9 +97,12 @@ def read(conn, mask):
         
     if data:
         print('echoing', repr(data), 'to', conn)
-        conn.send(data)  # Hope it won't block
+        #conn.send(data)  # Hope it won't block
         req=Request(data.decode())
         print("headers:",req.GetHeaders())
+        res = Response()
+        print("res:",res.GetResponse())
+        conn.send(bytes(res.GetResponse(), encoding='utf-8'))
         #print("body:",req.form_body())
     else:
         print('closing', conn)
@@ -96,7 +110,9 @@ def read(conn, mask):
         conn.close()
         
 def dataProc(data):
-    rspHttp="HTTP/1.0 200 OK\r\nSERVER: Dr.COM VER SERVER\r\nCONTENT-TYPE: %s\r\nCONTENT-LENGTH: %d\r\nCONECTION: CLOSE\r\n\r\n"
+    #rspHttp="HTTP/1.0 200 OK\r\nSERVER: Dr.COM VER SERVER\r\nCONTENT-TYPE: %s\r\nCONTENT-LENGTH: %d\r\nCONECTION: CLOSE\r\n\r\n"
+    pass
+    
 
 sel = selectors.DefaultSelector()
 
