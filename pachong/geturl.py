87f0d8d7405bb9316f87af55fa032ae048d2d62e
lich <lichzhenglz@gmail.com>
2019-03-18 09:45:10 +0800

    Update geturl.py

--- pachong/geturl.py
+++ pachong/geturl.py
@@ -2,12 +2,18 @@
 import urllib.request
 import re
 import os
-
+#import RemoteDisconnected
 def saveHtml(filename, html):
     with open(filename.replace('/', '_')+".html", "wb") as f:
         f.write(html)
         f.close()
 
-os.chdir("f:\python")
-url="http://127.0.0.1"
-html=urllib.request.urlopen(url)
+#os.chdir("f:\python")
+try:
+    url="http://192.168.54.37:8088/getvers?ver=1.1.1"
+    html=urllib.request.urlopen(url)
+    print(html)
+#except RemoteDisconnected as e:
+#    print(str(e))
+except ...:
+    print("error")
