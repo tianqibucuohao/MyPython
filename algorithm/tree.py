cbf4f64fdc03deba81665ddfe63b8821444b57fe
lich <lichzhenglz@gmail.com>
2019-04-02 17:44:22 +0800

    Update tree.py

--- algorithm/tree.py
+++ algorithm/tree.py
@@ -7,7 +7,16 @@ Created on Tue Apr  2 17:30:41 2019
 tree遍历方式：
 pre-order ：前序:父节点->左节点->右节点
 in-order：中序：左节点->父节点->右节点
-post-order:后序：左节点->右节点->父->
+post-order:后序：左节点->右节点->父节点
 
 """
 
+class TreeNode:
+    def __init__(self,x):
+        self.val = x
+        self.lef = None
+        self.right = None
+        
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:  
+        pass
