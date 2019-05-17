07d7e49e684c3035ec304446ead947bd7fc44b99
lich <lichzhenglz@gmail.com>
2019-05-17 16:49:34 +0800

    1
    1

--- cgi.py
+++ cgi.py
@@ -34,9 +34,9 @@ import cgitb
 cgitb.enable()
 # 不显示错误异常，转为保存到日志
 #cgitb.enable(display=0, logdir="/path/to/logdir")
-form = cgi.FieldStorage()
-if "name" not in form or "addr" not in form:
-    print("<H1>Error</H1>")
-    print("Please fill in the name and addr fields.")
-print("<p>name:", form["name"].value)
-print("<p>addr:", form["addr"].value)
+#form = cgi.FieldStorage()
+#if "name" not in form or "addr" not in form:
+#    print("<H1>Error</H1>")
+#    print("Please fill in the name and addr fields.")
+#print("<p>name:", form["name"].value)
+#print("<p>addr:", form["addr"].value)
---
+++ pachong/shahai.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 17 11:36:19 2019
+
+@author: Administrator
+"""
+
+import scrapy
+
+print("ok")
