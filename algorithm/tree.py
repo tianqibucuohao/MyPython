b2f9050e0b9bc7aa873ae9544ca6e569c8573b14
lich <lichzhenglz@gmail.com>
2019-10-03 09:24:13 +0800

    1
    1

--- algorithm/tree.py
+++ algorithm/tree.py
@@ -4,7 +4,7 @@ Created on Tue Apr  2 17:30:41 2019
 
 @author: Administrator
 
-tree遍历方式：
+二叉树tree遍历方式：
 pre-order ：前序:父节点->左节点->右节点
 in-order：中序：左节点->父节点->右节点
 post-order:后序：左节点->右节点->父节点
---
+++ numtest/mhelp.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+import os.path
+import webbrowser
+import subprocess
+import skimage
+import ffmpeg
+import scrapy
+
+
+var =help(scrapy.Selector)
+print((var))
+
+import socket
+
+var =help(socket.socket)
+print(len(var))
+
